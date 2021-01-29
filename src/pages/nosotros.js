422070d0079d630d854c788c6855f74aaf546c2b
ale.pissotti@gmail.com <ale.pissotti@gmail.com>
2021-01-29 11:40:45 -0300

    Agregando el componente de nosotros, obtener los datos del cms , darle estilos y mostrarlos en la página

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -2,11 +2,13 @@ import React from "react"
 
 //Components
 import Layout from '../components/layout';
+import Nosotros from '../components/nosotros';
 
 
 const IndexPage = () => (
   <Layout>
     <h1>Nosotros</h1>
+    <Nosotros/>
   </Layout>
 )
 
